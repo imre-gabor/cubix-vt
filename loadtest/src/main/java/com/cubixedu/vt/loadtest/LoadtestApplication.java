3551a1c19de591cdceac2eb3c4843a82e61c2ae6
imre-gabor <gabor@aut.bme.hu>
2024-04-10 16:42:55 +0200

    parse command line args and initialize RestClient

--- loadtest/src/main/java/com/cubixedu/vt/loadtest/LoadtestApplication.java
+++ loadtest/src/main/java/com/cubixedu/vt/loadtest/LoadtestApplication.java
@@ -1,13 +1,30 @@
 package com.cubixedu.vt.loadtest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
-public class LoadtestApplication {
+public class LoadtestApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+	private final RestClient.Builder restClientBuilder;
+	private RestClient restClient;
+
+    public LoadtestApplication(RestClient.Builder restClientBuilder) {
+        this.restClientBuilder = restClientBuilder;
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(LoadtestApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		int numThreads = Integer.parseInt(args[0]);
+		int numRequests = Integer.parseInt(args[1]);
+		String uri = args[2];
+		restClient = restClientBuilder.baseUrl(uri).build();
+	}
 }
